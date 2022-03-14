f6aaca7c02f6c6f5b25416f4c3456fd509ee5c20
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-03-13 23:38:21 -0400

    create input fields for book info, JS skeleton

--- library.js
+++ library.js
@@ -1,18 +1,16 @@
+let myLibrary = []
+
 function Book(title,author,pages,read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        infoStr = title + " by " + author + ", " + pages + " pages, " + read;
-        return infoStr;
-    }
-}
 
-const theHobbit = new Book(
-    "The Hobbit",
-    "J.R.R. Tolkien",
-    295,
-    "not read yet");
-console.log("The Odin Project - Book Constructor")
-console.log(theHobbit.info())
+}
+Book.prototype.info = function(){
+    return (title + " by " + author + ", " + pages + " pages, " + read);
+} 
+
+function addBookToLibrary(){
+
+}
